c872285ec1cc64151889c2a67e7c36e7461f5a4a
Sta11k <alexandiel1984@gmail.com>
2021-10-10 17:48:19 +0300

    murkup

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -2,12 +2,6 @@ import React from 'react';
 import styles from './Feedback.module.css';
 import { Component } from 'react';
 
-// export default function Feedback(props) {
-//   //  const { id, label, percentage } = props;
-
-//   return <p className={styles.stales}>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>;
-// }
-
 const ButtonGood = ({ changeMessage, label }) => (
   <button type="button" onClick={changeMessage}>
     {label}
